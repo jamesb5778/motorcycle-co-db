2b08fd778f29712e4b0aaef7ab0b84bbb1d58496
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-11 22:10:23 -0700

    add PASSWORD to connect database and run migrations

--- motorcycle_co_db/settings.py
+++ motorcycle_co_db/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'motorcycle_co_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'motorcycle_co',
+        'PASSWORD': 'Mosby',
     }
 }
 
