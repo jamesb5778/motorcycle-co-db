9ceb6f67693546a677625f6e0fbf379aed184990
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-13 16:31:26 -0700

    add base.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
 # Create your views here.
@@ -9,6 +8,5 @@ class Home(TemplateView):
     template_name = "home.html"
 
 #view for the motorcycle company list
-class Motorcycle_Co_List(View):
-    def get(self, request):
-        return HttpResponse("List of Motorcycle Companies")
+class Motorcycle_Co_List(TemplateView):
+    template_name = "motorcycle_co_list.html"
