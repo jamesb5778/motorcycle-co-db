668527666078bfc5dd36e6f8d1bf9c304d62c785
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-16 18:07:55 -0700

    update page working

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path('', views.Home.as_view(), name = "home"),
     # path for motorcycle company list page
     path('motorcycle_co_list/', views.Motorcycle_Co_List.as_view(), name = "motorcycle_co_list"),
-    # path for single motorcycle company. List all the motorcycle models
+    #path to add motorcycle company to motorcycle list
+    path('motorcycle_co_list/new', views.Motorcycle_Co_Create.as_view(), name = "motorcycle_co_create"),
+    # path for details page of the company, motorcycle models will go here
+    path('motorcycle_co_list/<int:pk>/', views.Motorcycle_Co_Details.as_view(), name = "motorcycle_co_details"),
+    # path to update motorcycle company
+    path('motorcycle_co_list/<int:pk>/update', views. Motorcycle_Co_Update.as_view(), name = "motorcycle_co_update"),
     # path for single model of motorcycle
-    
-    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,46 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
 from .models import Motorcycle_Company
 
 # Create your views here.
 #view for the homepage
 class Home(TemplateView):
     template_name = "home.html"
-
+    
 #view for the motorcycle company list
 class Motorcycle_Co_List(TemplateView):
     template_name = "motorcycle_co_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["motorcycle_company"] = Motorcycle_Company.objects.all()
+        name = self.request.GET.get("name")
+        if name != None:
+            context["motorcycle_company"] = Motorcycle_Company.objects.filter(name__icontains=name)
+            context["header"] = f"Searching for {name}"
+        else:
+            context["motorcycle_company"] = Motorcycle_Company.objects.all()
+            context["header"] = "Motorcycle Companies"
         return context
+    
+# view to create/add a motorcycle company to motorcycle_co_list
+class Motorcycle_Co_Create(CreateView):
+    model = Motorcycle_Company
+    fields = ['name', 'image', 'founded', 'description']
+    template_name = "motorcycle_co_create.html"
+    success_url = "/motorcycle_co_list/"
+
+# view for Motorcycle company details
+class Motorcycle_Co_Details(DetailView):
+    model = Motorcycle_Company
+    template_name = "motorcycle_co_details.html"
+
+# view for Motorcycle company update
+class Motorcycle_Co_Update(UpdateView):
+    model = Motorcycle_Company
+    fields = ['name', 'image', 'founded', 'description']
+    template_name = "motorcycle_co_update.html"
+    success_url = "/motorcycle_co_list/"
