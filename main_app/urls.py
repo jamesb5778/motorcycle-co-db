f02ab1a2eccf565275ca977456b9a2fc00c37c34
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-18 13:30:32 -0700

    made page for all the motorcycle models to be displayed.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,16 +4,28 @@ from . import views
 urlpatterns = [
     # path for the home page.
     path('', views.Home.as_view(), name = "home"),
+    
     # path for motorcycle company list page
     path('motorcycle_co_list/', views.Motorcycle_Co_List.as_view(), name = "motorcycle_co_list"),
+    
     #path to add motorcycle company to motorcycle list
     path('motorcycle_co_list/new', views.Motorcycle_Co_Create.as_view(), name = "motorcycle_co_create"),
+    
     # path for details page of the company, motorcycle models will go here
     path('motorcycle_co_list/<int:pk>/', views.Motorcycle_Co_Details.as_view(), name = "motorcycle_co_details"),
+    
     # path to update motorcycle company
     path('motorcycle_co_list/<int:pk>/update', views. Motorcycle_Co_Update.as_view(), name = "motorcycle_co_update"),
+    
     # path for deleting motorcycle company
     path('motorcycle_co_list/<int:pk>/delete', views.Motorcycle_Co_Delete.as_view(), name = "motorcycle_co_delete"),
+    
+    #path to see a list of all the motorcycle models from every company
+    path('motorcycle_model_list/', views.Motorcycle_Model_List.as_view(), name = "motorcycle_model_list"),
+    
     #path to see individual motorcycle models.
-    path('motorcycle_model_details/<int:pk>/', views.Motorcycle_Model_Details.as_view(), name = "motorcycle_model_details"),
+    #path('motorcycle_model_details/<int:pk>/', views.Motorcycle_Model_Details.as_view(), name = "motorcycle_model_details"),
+    
+    # path to add a motorcycle to the Motorcycle_Company
+    #path('motorcycle_co_list/<int:pk>/mc-model/new', views.Motorcycle_Model_Create.as_view(), name = "motorcycle_model_create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import DeleteView
 from django.urls import reverse
 from .models import Motorcycle_Company, Motorcycle_Model
 
@@ -56,8 +54,18 @@ class Motorcycle_Co_Delete(DeleteView):
     model = Motorcycle_Company
     template_name = "motorcycle_co_delete_confirmation.html"
     success_url = "/motorcycle_co_list/"
+
+# class to view all the motorcycles from all the companies 
+class Motorcycle_Model_List(TemplateView):
+    template_name = "motorcycle_model_list.html"
     
 #class to view a single motorcycle model or the details of that model after being clicked
 class Motorcycle_Model_Details(DetailView):
     model = Motorcycle_Model
     template_name = "motorcycle_model_details.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["mc_models"] = []
+        return context
+# class to add Motorcycle Models onto the
